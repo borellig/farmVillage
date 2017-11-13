5fbff2f3afd7826b2ee85916a1ce18b12c0c0cc7
hm3il <hadj.kherif@gmail.com>
2017-11-13 17:28:34 +0100

    13-11 missoum : ajout fonctionnalite facebook connect

--- FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
+++ FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
@@ -1,14 +1,22 @@
 package com.android.group.farmvillage.Activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import com.android.group.farmvillage.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -33,6 +43,30 @@ public class MainActivity extends AppCompatActivity {
 
         /*Passage activité Login pour test */
 
+
     }
+
+    /**
+     * Fonction pour récupérer la clé SHA1 pour FB Connect
+     */
+    private void recuperCleSha1(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.android.group.farmvillage",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+    }
+
+
 
 }
