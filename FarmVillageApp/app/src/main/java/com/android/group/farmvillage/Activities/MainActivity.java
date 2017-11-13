853cf04f4e2edb3db08a67eee69b6cf2b8c0d7a6
borellig <geoffrey.borelli@gmail.com>
2017-11-13 09:24:20 +0100

    structure

--- FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
+++ FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.android.group.farmvillage;
+package com.android.group.farmvillage.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.group.farmvillage.R;
 
 public class MainActivity extends AppCompatActivity {
 
