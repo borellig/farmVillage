05203bb4523b2c01037a9a294ffee150eef6768e
unknown <guillaume.richd@gmail.com>
2017-11-13 18:16:08 +0100

    13/11/17

--- FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
+++ FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -24,27 +27,65 @@ public class MainActivity extends AppCompatActivity {
     //Déclaration de l'adapteur MarketAdapteur
     public map_adapt mapAdapteur;
 
+    /**
+     * Créé le menu horizontal en haut du layout
+     * @param menu Le contenu du menu
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_test à l'ActionBar
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
         ArrayList<Coordonnees> coord = new ArrayList<Coordonnees>();
         coord.add(new Coordonnees(1, 1));
         Date d = new Date();
 
-        Building b1 = new Building(true, 1, TypeBuilding.HDV, coord, d, 50);
+        final Building b1 = new Building(true, 1, TypeBuilding.HDV, coord, d, 50);
+        final Building b2 = new Building(true, 2, TypeBuilding.Ferme, coord, d, 50);
+        final Building b3 = new Building(true, 3, TypeBuilding.Entrepot, coord, d, 50);
+        final Building b4 = new Building(true, 4, TypeBuilding.Champs, coord, d, 50);
+        final Building b5 = new Building(true, 5, TypeBuilding.HDV, coord, d, 50);
+        final Building b6 = new Building(true, 1, TypeBuilding.HDV, coord, d, 50);
+        final Building b7 = new Building(true, 2, TypeBuilding.Ferme, coord, d, 50);
+        final Building b8 = new Building(true, 3, TypeBuilding.Entrepot, coord, d, 50);
+        final Building b9 = new Building(true, 4, TypeBuilding.Champs, coord, d, 50);
+        final Building b10 = new Building(true, 5, TypeBuilding.HDV, coord, d, 50);
+        final Building b11= new Building(true, 1, TypeBuilding.HDV, coord, d, 50);
+        final Building b12= new Building(true, 2, TypeBuilding.Ferme, coord, d, 50);
+        final Building b13= new Building(true, 3, TypeBuilding.Entrepot, coord, d, 50);
+        final Building b14= new Building(true, 4, TypeBuilding.Champs, coord, d, 50);
+        final Building b15= new Building(true, 5, TypeBuilding.HDV, coord, d, 50);
 
         final ArrayList<Building> listBatiment = new ArrayList<Building>(24);
         for (int i=0; i<24; i++){
             listBatiment.add(i, null);
         }
         listBatiment.set(0, b1);
-
-
-
+        listBatiment.set(1, b2);
+        listBatiment.set(2, b3);
+        listBatiment.set(3, b4);
+        listBatiment.set(4, b5);
+        listBatiment.set(5, b6);
+        listBatiment.set(6, b7);
+        listBatiment.set(7, b8);
+        listBatiment.set(8, b9);
+        listBatiment.set(9, b10);
+        listBatiment.set(10, b11);
+        listBatiment.set(11, b12);
+        listBatiment.set(12, b13);
+        listBatiment.set(13, b14);
+        listBatiment.set(14, b15);
 
 
 
@@ -64,7 +105,8 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.d("position", String.valueOf(position));
                 if (listBatiment.get(position)==null){
-                    Log.d("vide", "ajoute building");
+                    listBatiment.set(position, b1);
+                    mapAdapteur.notifyDataSetChanged();
                 }
                 else{
                     Log.d("vide", listBatiment.get(position).getsName());
