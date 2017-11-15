722bf3445ab25d06cc363ec265049e885f12e75e
hm3il <hadj.kherif@gmail.com>
2017-11-15 23:54:25 +0100

    15-11 Missoum , modif exchange activity, ajout class comparaison nombre

--- FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/ExchangeActivity.java
+++ FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/ExchangeActivity.java
@@ -2,9 +2,11 @@ package com.android.group.farmvillage.Activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
@@ -13,9 +15,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.group.farmvillage.Adapteur.exchange_adap;
 import com.android.group.farmvillage.Modele.AskExchange;
+import com.android.group.farmvillage.Modele.InputFilterMinMax;
 import com.android.group.farmvillage.Modele.Ressource;
 import com.android.group.farmvillage.Modele.Village;
 import com.android.group.farmvillage.R;
@@ -37,8 +41,7 @@ public class ExchangeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
         Intent i = getIntent();
-        Village Myvillage = (Village) i.getSerializableExtra(VillageIntent);
-
+        final Village Myvillage = (Village) i.getSerializableExtra(VillageIntent);
         //Mise en relation Layout Object
 
         mListView = (ListView) findViewById(R.id.ExchangeListeView);
@@ -57,7 +60,7 @@ public class ExchangeActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         AskExchange request = (AskExchange) adapterView.getItemAtPosition(i);
-                        sendRessource(request);
+                        sendRessource(request,Myvillage,1);
                     }
                 });
             }
@@ -68,7 +71,13 @@ public class ExchangeActivity extends AppCompatActivity {
                 List<AskExchange>  listAsk2 =  TryForAttribut("Or",listAsk);
                 exchange_adap adapter = new exchange_adap(ExchangeActivity.this, listAsk2);
                 mListView.setAdapter(adapter);
-
+                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        AskExchange request = (AskExchange) adapterView.getItemAtPosition(i);
+                        sendRessource(request,Myvillage,3);
+                    }
+                });
             }
         });
 
@@ -78,6 +87,13 @@ public class ExchangeActivity extends AppCompatActivity {
                 List<AskExchange>  listAsk2 =  TryForAttribut("Pierre",listAsk);
                 exchange_adap adapter = new exchange_adap(ExchangeActivity.this, listAsk2);
                 mListView.setAdapter(adapter);
+                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        AskExchange request = (AskExchange) adapterView.getItemAtPosition(i);
+                        sendRessource(request,Myvillage,4);
+                    }
+                });
             }
         });
         FoodButton.setOnClickListener(new View.OnClickListener() {
@@ -86,6 +102,13 @@ public class ExchangeActivity extends AppCompatActivity {
                 List<AskExchange>  listAsk2 =  TryForAttribut("Food",listAsk);
                 exchange_adap adapter = new exchange_adap(ExchangeActivity.this, listAsk2);
                 mListView.setAdapter(adapter);
+                mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        AskExchange request = (AskExchange) adapterView.getItemAtPosition(i);
+                        sendRessource(request,Myvillage,2);
+                    }
+                });
             }
         });
 
@@ -113,7 +136,7 @@ public class ExchangeActivity extends AppCompatActivity {
         request.add(new AskExchange("Henry",ressource3));
         Ressource ressource4 = new Ressource("Pierre",4);
         request.add(new AskExchange("Henry",ressource4));
-        Ressource ressource5 = new Ressource("Bois",4);
+        Ressource ressource5 = new Ressource("Bois",3444);
         request.add(new AskExchange("Zizou",ressource5));
         Ressource ressource6 = new Ressource("Bois",4);
         request.add(new AskExchange("Zizou",ressource6));
@@ -138,26 +161,50 @@ public class ExchangeActivity extends AppCompatActivity {
         return  request;
     }
 
-    private void sendRessource(AskExchange request){
+    private void sendRessource(AskExchange request, Village village, int i){
+        final int max;
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Redemander votre mots de passe");
+        builder.setTitle("Gestion d'envoi de ressource");
+
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
-        //final DBHandler db1 = db;
 
-        int qtiterequest = request.getrRessource().getQte();
+        max = ComparaisonValeurRessource(request, village,i);
 
         final EditText titleBox = new EditText(this);
-        titleBox.setText(String.valueOf(request.getrRessource().getQte()));
+        switch(i){
+            case 1:
+                if(max==village.getiWood()){
+                    titleBox.setTextColor(Color.RED);
+                }
+                break;
+            case 2:
+                if(max==village.getiFood()){
+                    titleBox.setTextColor(Color.RED);
+                }
+                break;
+            case 3:
+                if(max==village.getiGold()){
+                    titleBox.setTextColor(Color.RED);
+                }
+                break;
+            case 4:
+                if(max==village.getiRock()){
+                    titleBox.setTextColor(Color.RED);
+                }
+                break;
+        }
+        titleBox.setText(String.valueOf(max));
         titleBox.setInputType(InputType.TYPE_CLASS_NUMBER);
         layout.addView(titleBox);
         builder.setView(layout);
+        titleBox.setFilters(new InputFilter[]{ new InputFilterMinMax("0", String.valueOf(max))});
+        //Controle de saisie
+
         // Configurations des bouttons
         builder.setPositiveButton("Total", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
-
 
             }
         });
@@ -171,12 +218,61 @@ public class ExchangeActivity extends AppCompatActivity {
         builder.setNeutralButton("Valider", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
+                if(Integer.parseInt(titleBox.getText().toString())>max){
+                    titleBox.setText(String.valueOf(max));
+                    Toast.makeText(getApplicationContext(), "La quantité indiquée est trop élevée : "+max+" et pas plus", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         builder.show();
+    }
 
+    /**
+     *
+     * @param request information de la demande
+     * @param village objet associé au village de l'utilisateur
+     * @param i indicateur pour savoir la demande (or,bois,pierre ou nourriture)
+     * @return
+     */
+    private int ComparaisonValeurRessource(AskExchange request, Village village, int i){
+        int max=0;
+        switch (i) {
+            case 1:
+                if(request.getrRessource().getQte()>=village.getiWood()){
+                    max = village.getiWood();
+                }
+                else {
+                    max = request.getrRessource().getQte();
+                }
+
+                break;
+            case 2:
+                if(request.getrRessource().getQte()>=village.getiFood()){
+                    max = village.getiFood();
+                }
+                else {
+                    max = request.getrRessource().getQte();
+                }
+                break;
+            case 3:
+                if(request.getrRessource().getQte()>=village.getiGold()){
+                    max = village.getiGold();
+                }
+                else {
+                    max = request.getrRessource().getQte();
+                }
+                break;
+            case 4:
+                if(request.getrRessource().getQte()>=village.getiRock()){
+                    max = village.getiRock();
+                }
+                else {
+                    max = request.getrRessource().getQte();
+                }
+                break;
+        }
+        return max;
 
     }
 
