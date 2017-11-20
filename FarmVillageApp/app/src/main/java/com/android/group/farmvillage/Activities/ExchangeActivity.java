a0f2b86d1476a3a9d540fbf0e6a2264c10483d7f
borellig <geoffrey.borelli@gmail.com>
2017-11-20 15:45:30 +0100

    20/11/17 - appel village

--- FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/ExchangeActivity.java
+++ FarmVillageApp/app/src/main/java/com/android/group/farmvillage/Activities/ExchangeActivity.java
@@ -34,10 +34,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.Call;
-import okhttp3.Callback;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.Response;
 
 import static java.sql.DriverManager.println;
